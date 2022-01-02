9a75990fd32c262e8921e8847afef7aaef9eae30
Hamid Oujand <hmdrzaa11@gmail.com>
2022-01-02 14:53:01 +0330

    last minute change

--- auth/src/routes/__tests__/current-user.test.ts
+++ auth/src/routes/__tests__/current-user.test.ts
@@ -7,7 +7,7 @@ it("should response with details about the current user", async () => {
     .get("/api/users/currentuser")
     .set("Cookie", cookies)
     .send()
-    .expect(200);
+    .expect(400);
 
   expect(res.body.currentUser.email).toEqual("asdf@asdf.com");
 });
